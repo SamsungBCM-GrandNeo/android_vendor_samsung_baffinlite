9c94eefc40050b748d10fbc64d1a74f24e972135
LuK1337 <priv.luk@gmail.com>
2014-09-15 14:20:49 +0200

    Add missing AT libs

--- baffinlite-vendor-blobs.mk
+++ baffinlite-vendor-blobs.mk
@@ -61,6 +61,9 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/baffinlite/proprietary/lib/lib_SamsungRec_V03011b.so:system/lib/lib_SamsungRec_V03011b.so \
     vendor/samsung/baffinlite/proprietary/lib/lib_SoundAlive_V01013q.so:system/lib/lib_SoundAlive_V01013q.so \
     vendor/samsung/baffinlite/proprietary/lib/libat.so:system/lib/libat.so \
+    vendor/samsung/baffinlite/proprietary/lib/libatparser.so:system/lib/libatparser.so \
+    vendor/samsung/baffinlite/proprietary/lib/libomission_avoidance.so:system/lib/libomission_avoidance.so \
+    vendor/samsung/baffinlite/proprietary/lib/libfactoryutil.so:system/lib/libfactoryutil.so \
     vendor/samsung/baffinlite/proprietary/lib/libbralloc.so:system/lib/libbralloc.so \
     vendor/samsung/baffinlite/proprietary/lib/libuip.so:system/lib/libuip.so \
     vendor/samsung/baffinlite/proprietary/lib/libat_stubs.so:system/lib/libat_stubs.so \
